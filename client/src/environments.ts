2a465b8e29e6d95020e3a6d2b989ce89cb472097
ELOWIS37 <eloy.castano@institutvidreres.cat>
2023-03-09 08:40:00 +0100

    commit2

--- client/src/environments.ts
+++ client/src/environments.ts
@@ -1,12 +1,13 @@
+
 export const environment = {
     production: false,
     firebaseConfig: {
-      apiKey: '<API_KEY>',
-      authDomain: 'inicisessio-115ad.firebaseapp.com',
-      databaseURL: 'https://inicisessio-115ad.firebaseio.com',
-      projectId: 'inicisessio-115ad',
-      storageBucket: '<BUCKET>.appspot.com',
-      messagingSenderId: '<SENDER_ID>',
-      appId: '<APP_ID>',
+      apiKey: "AIzaSyCn-EgfMKUr8fnjPEHk6PEUXjZVtocc6Is",
+      authDomain: "inici-sessio.firebaseapp.com",
+      databaseURL: 'https://inici-sessio.firebaseio.com',
+      projectId: "inici-sessio",
+      storageBucket: "inici-sessio.appspot.com",
+      messagingSenderId: "192534858921",
+      appId: "1:192534858921:web:e1a7b8bb67876a359f2db8"
     }
   };
